5a0c7e88d5a9286ec74291c3052f859445604df0
Mikael Karlsson <i8myshoes@gmail.com>
2014-03-18 13:24:40 +0000

    Reformatted setup.py
    git-svn-id: https://svn.eudat.eu/EUDAT/Services/MetaData/ckanext-datesearch@2904 68e52488-0a15-44bc-a314-416658652264

--- setup.py
+++ setup.py
@@ -1,32 +1,31 @@
 from setuptools import setup, find_packages
-import sys, os
 
-version = '0.0'
+version = '0.1'
 
 setup(
-	name='ckanext-datesearch',
-	version=version,
-	description="",
-	long_description="""\
-	""",
-	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-	keywords='',
-	author='',
-	author_email='',
-	url='',
-	license='',
-	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-	namespace_packages=['ckanext', 'ckanext.datesearch'],
-	include_package_data=True,
-	zip_safe=False,
-	install_requires=[
-		# -*- Extra requirements: -*-
-	],
-	entry_points=\
-	"""
+    name='ckanext-datesearch',
+    version=version,
+    description="",
+    long_description="""\
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.datesearch'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points=\
+    """
         [ckan.plugins]
-	# Add plugins here, eg
-	# myplugin=ckanext.datesearch:PluginClass
+    # Add plugins here, eg
+    # myplugin=ckanext.datesearch:PluginClass
     datesearch=ckanext.datesearch.plugin:DateSearchPlugin
-	""",
+    """,
 )
