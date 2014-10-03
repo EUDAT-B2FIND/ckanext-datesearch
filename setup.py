0ebdbac553eab17606a550b6fdf22f762a608bdf
Mikael Karlsson <i8myshoes@gmail.com>
2014-10-03 14:11:46 +0300

    Clean up setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     """
     """,
     classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
+    keywords='ckan ckanext datesearch facet',
+    author='Mikael Karlsson',
+    author_email='i8myshoes@gmail.com',
+    url='https://github.com/B2FIND/ckanext-datesearch',
+    license='AGPLv3',
     packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
     namespace_packages=['ckanext', 'ckanext.datesearch'],
     include_package_data=True,
@@ -25,8 +25,6 @@ setup(
     entry_points=
     """
     [ckan.plugins]
-    # Add plugins here, eg
-    # myplugin=ckanext.datesearch:PluginClass
     datesearch=ckanext.datesearch.plugin:DateSearchPlugin
     """,
 )
