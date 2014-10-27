9ae8ec562f3278dcf18708b3e70a82f0d5b664c6
Mikael Karlsson <i8myshoes@gmail.com>
2014-10-27 16:05:08 +0200

    Use publication timestamp field

--- ckanext/datesearch/plugin.py
+++ ckanext/datesearch/plugin.py
@@ -38,7 +38,7 @@ class DateSearchPlugin(plugins.SingletonPlugin):
         # Add a date-range query with the selected start and/or end dates into the Solr facet queries.
         fq = search_params['fq']
         log.debug("fq: {0}".format(fq))
-        fq = '{fq} +metadata_modified:[{sd} TO {ed}]'.format(fq=fq, sd=start_date, ed=end_date)
+        fq = '{fq} +extras_PublicationTimestamp:[{sd} TO {ed}]'.format(fq=fq, sd=start_date, ed=end_date)
 
         log.debug("fq: {0}".format(fq))
         search_params['fq'] = fq
