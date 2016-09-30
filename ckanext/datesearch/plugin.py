0bab1b6487b453186482f257f9c7f0c87fe3c8a6
Mikael Karlsson <i8myshoes@gmail.com>
2016-09-30 18:00:12 +0300

    Remove debug messages

--- ckanext/datesearch/plugin.py
+++ ckanext/datesearch/plugin.py
@@ -16,16 +16,13 @@ class DateSearchPlugin(plugins.SingletonPlugin):
 
     def before_search(self, search_params):
         extras = search_params.get('extras')
-        log.debug("extras: {0}".format(extras))
         if not extras:
             # There are no extras in the search params, so do nothing.
             return search_params
 
         start_date = extras.get('ext_startdate')
-        log.debug("start_date: {0}".format(start_date))
 
         end_date = extras.get('ext_enddate')
-        log.debug("end_date: {0}".format(end_date))
 
         if not start_date and not end_date:
             # The user didn't select either a start and/or end date, so do nothing.
@@ -37,11 +34,8 @@ class DateSearchPlugin(plugins.SingletonPlugin):
 
         # Add a date-range query with the selected start and/or end dates into the Solr facet queries.
         fq = search_params['fq']
-        log.debug("fq: {0}".format(fq))
         fq = '{fq} +extras_PublicationTimestamp:[{sd} TO {ed}]'.format(fq=fq, sd=start_date, ed=end_date)
 
-        log.debug("fq: {0}".format(fq))
         search_params['fq'] = fq
-        log.debug("search_params: {0}".format(search_params))
 
         return search_params
